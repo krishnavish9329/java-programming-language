e89bdb419a0a7e9d2b0b50c5707e31493959eecc
Krishna Kumar Vishwakarma <108523587+krishnavish9329@users.noreply.github.com>
2022-12-24 21:00:30 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,23 @@
+// java-programming-language
+
+public class data_type
+{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    public static void main(String[] arre) 
+    {
+        System.out.print("hello krishna");
+        int s=6; //it int datatype
+        float F=45f; // if you want to float type. so you write f at last of number. and befoult it double  
+        long L=33;  //it long datatype
+        double D=3323232; //it double dtatype
+        char CH='k'; // it charecter dattype
+        String st="i'm learning java language"; //it String datatype
+        boolean b=true; // it booleam=n datatype
+        System.out.println(s);
+        System.out.println(F);
+        System.out.println(L);
+        System.out.println(D);
+        System.out.println(CH);
+        System.out.println(st);
+        System.out.println(b);
+    }
+}
