bd4872214dbd8a5ebc9d92c1bd5398eb11984f0e
Krishna Kumar Vishwakarma <108523587+krishnavish9329@users.noreply.github.com>
2022-12-24 20:38:46 +0530

    Create basic-structure.java

---
+++ basic-structure.java
@@ -0,0 +1,17 @@
+// java programming 
+class simple {
+    public static void main(String[] ag)
+    {
+        System.out.println("Hello world!");
+    } 
+}
+
+/*
+* save this java file  as simple.java
+* CLASS: class is a keyword is used to declare a class in java
+* PUBLIC: public keyword is an access modifier which represents visibility it means it is visible to all
+* VOID: void is return type of method but it don't return any value
+* MAIN: main represent startup of the program.
+* String[] args: String[] array is used for command line argument.
+* System.out.println: it is used print statement.
+*/
