8cec28b4499030070fdf439c7210b55937f0fb76
Krishna Kumar Vishwakarma <108523587+krishnavish9329@users.noreply.github.com>
2022-12-25 09:18:52 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,56 @@
+//# java-programming-language
+
+import java.util.Scanner;
+public class array
+{
+    /*public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int []arr= new int[5];
+        System.out.println("enter the array:");
+        for(int i=0;i<5;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        System.out.println("print the array:");
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }*/
+    
+    /*
+    public static void main(String[] args) {
+       // Scanner s= new Scanner(System.in);
+       // int []arr= new int[5];
+       int arr[]={2,4,6,8,10,12};
+       for(int i=arr.length-1; i>=0 ;i--)
+       {
+        System.out.println(arr[i]);
+       }
+
+    }*/
+   /* public static void main(String[] args) {
+        // Scanner s= new Scanner(System.in);
+        // int []arr= new int[5];
+        int arr[]={2,4,6,8,10,12};
+        for(int ele : arr)
+        {
+         System.out.println(ele);
+        }
+        
+     }*/
+     //2_d array
+     public static void main(String[] args) {
+        // Scanner s= new Scanner(System.in);
+        int []arr[]= new int[5][2];
+        //int arr[]={2,4,6,8,10,12};
+        /*for(int i=arr.length-1; i>=0 ;i--)
+        {
+        
+         System.out.println(arr[i]);
+        }*/
+        System.out.println(arr.length);
+ 
+     }
+}
