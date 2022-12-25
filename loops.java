141378228ea139181b57c5d2cbbf21707ab41dd5
Krishna Kumar Vishwakarma <108523587+krishnavish9329@users.noreply.github.com>
2022-12-25 09:13:41 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,24 @@
+//# java-programming-language
+
+public class loop
+{
+    public static void main(String arge[])
+    {
+        /*for(int i=100;i<=200;i++)  //it's for loop
+        {
+            System.out.println(i);
+        }*/
+       /* int j=100;
+        while(j!=201)  // it's while loop
+        {
+            System.out.println(j);
+            j++;
+        }*/
+        int k=101;
+        do{                     // it's do{ ...} while loop
+            System.out.println(k);
+            k++;
+        }while(k!=201);
+    }
+  
+}
